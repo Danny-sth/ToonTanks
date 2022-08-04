d37be5a72bf28c73a0bbf4f621a46a15d8014fb3
Denis Kudinov <danny.wasth@gmail.com>
2022-08-04 20:06:48 +0300

    add PawnTurret c++ class

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -23,8 +23,10 @@ class TOONTANKS_API APawnTank : public APawnBase
 
 	FVector MoveDirection;
 	FQuat RotationDirection;
-	float MoveSpeed = 100.0f;
-	float RotateSpeed = 100.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta=(AllowPrivateAccess = "true"))
+	float MoveSpeed = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta=(AllowPrivateAccess = "true"))
+	float RotateSpeed = 250.0f;
 
 	void CalculateMoveInput(float value);
 	void CalculateRotateInput(float value);
---
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTurret.h"
+
+void APawnTurret::CheckFireCondition()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Fire Condition Checked"))
+}
+
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager()
+	          .SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+}
